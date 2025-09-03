e69f40e499af95f4a76f9a790b5327e86744382a
oberoi11 <oberoiansh899@gmail.com>
2025-09-04 01:36:36 +0530

    Update axiosInstance.js

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: "https://citybus-wwml.onrender.com",
 });
 
 // ✅ Attach token dynamically
