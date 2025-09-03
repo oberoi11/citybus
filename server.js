3e04e643d5bee9b0cd5b680837b8685b48e85eda
oberoi11 <oberoiansh899@gmail.com>
2025-09-03 23:49:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
+const path=require("path")
 
 const corsOptions = {
   origin: "http://localhost:3000",
