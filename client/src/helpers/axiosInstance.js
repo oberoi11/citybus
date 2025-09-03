58190e22e6083e54fea630e9a960c336c4df2713
oberoi11 <oberoiansh899@gmail.com>
2025-09-04 01:02:55 +0530

    Update axiosInstance.js

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`
-  },
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
 });
+
+// ✅ Attach token dynamically (so it always uses the latest one)
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
