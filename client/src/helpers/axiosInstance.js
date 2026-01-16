ec6d20ceea8a50debad1cf4a18d1e618dd3696ec
oberoi11 <oberoiansh899@gmail.com>
2026-01-16 17:55:00 +0530

    Update axiosInstance.js

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://citybus-1.onrender.com",
+  baseURL: "https://citybus-hjup.onrender.com",
 });
 
 // ✅ Attach token dynamically
